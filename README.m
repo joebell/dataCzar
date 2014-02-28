924ea4c3bfe4e3963b87fc4d130bf0af353d7ab8
Joe Bell <joe.bell@gmail.com>
2014-02-28 11:37:33 -0500

    Added some documentation

---
+++ README.m
@@ -0,0 +1,28 @@
+%%
+%   microCzar
+%
+%   microCzar is a lightweight MATLAB package for organizing data and code.
+%   It provides a simple interface to existing tools, and focuses on not
+%   duplicating their functionality.
+%
+%   Requirements:
+%
+%       To use microCzar you'll need to do some system setup. It varies by
+%       type of system, but will work in Windows, Mac, and Linux. 
+%
+%       git - Git is a version control system that you should use to keep
+%       track of your code. You can download it here: 
+%           http://git-scm.com/downloads
+%       You should also setup an account on GitHub, a free service that
+%       works with git to keep off-site backups of your code.
+%           http://www.github.com
+%
+%       rsync - Rsync is a powerful file synchronization tool that compares
+%       local and remote directories, and transmits only parts of files
+%       that have changed.
+%           http://rsync.samba.org/download.html
+%
+%       ssh - SSH is a program for secure remote logins. git and rsync both
+%       use SSH to allow you to login to your backup servers without typing
+%       in a password.
+%
